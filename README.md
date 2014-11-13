ff88228f3f9ba19fc35476258b9e7afdde1d453a
Mariusz Nowak <medyk@medikoo.com>
2014-11-13 11:47:46 +0100

    Initial

---
+++ README.md
@@ -0,0 +1,30 @@
+# i18n2-md-to-dom
+
+### Installation
+
+	$ npm install medikoo/i18n2-md2dom
+
+### Usage
+
+Converts [i18n2](https://github.com/medikoo/i18n2#i18n2) message, supposedly written with markdown format into DOM
+
+
+```javascript
+var _ = require('i18n2')(locale);
+var md2dom = require(i18n2-md-to-dom);
+
+var dom = md2Dom(_("Some text with __important__ content."));
+// dom reflects: <p>Some text with <strong>important</strong> content.</p>
+
+// Inserts are fully supported
+dom = md2Dom(_("My name is __${ fullName }__.", {
+  fullName: "John Smith"
+}));
+// dom reflects: <p>My name is <strong>John Smith</strong>.</p>
+```
+
+Behind the scenes it uses [Remarkable](https://github.com/jonschlinkert/remarkable#remarkable) markdown parser
+
+### Tests [![Build Status](https://travis-ci.org/medikoo/i18n2-md-to-dom.svg)](https://travis-ci.org/medikoo/i18n2-md-to-dom)
+
+	$ npm test
---
+++ test/index.js
@@ -0,0 +1,54 @@
+'use strict';
+
+var i18n2 = require('i18n2');
+
+module.exports = function (t, a) {
+	var _ = i18n2({ foo: 'bar' }), md = t(document), result;
+
+	a.h1("Plain text");
+	result = md(_('razdwa'));
+	a(result.nodeName, 'P');
+	a(result.childNodes.length, 1);
+	a(result.firstChild.data, 'razdwa');
+
+	a.h1("Mixed");
+	result = md(_('raz __elo__ dwa'));
+	a(result.nodeName, 'P');
+	a(result.childNodes.length, 3);
+	a(result.childNodes[0].data, 'raz ');
+	a(result.childNodes[1].nodeName, 'STRONG');
+	a(result.childNodes[1].firstChild.data, 'elo');
+	a(result.childNodes[2].data, ' dwa');
+
+	a.h1("Plain with variables");
+	result = md(_('marko ${ miszel } zagalo', { miszel: 'fura' }));
+	a(result.nodeName, 'P');
+	a(result.childNodes.length, 3);
+	a(result.childNodes[0].data, 'marko ');
+	a(result.childNodes[1].data, 'fura');
+	a(result.childNodes[2].data, ' zagalo');
+
+	a.h1("Mixed with varabiles");
+	result = md(_('marko __marek ${ miszel } zegarek__ zagalo ${ fuszka }', {
+		miszel: 'fura',
+		fuszka: {
+			toDOM: function (document) {
+				var el = document.createElement('p');
+				el.innerHTML = 'razdwa';
+				return el;
+			}
+		}
+	}));
+	a(result.nodeName, 'P');
+	a(result.childNodes.length, 4);
+	a(result.childNodes[0].data, 'marko ');
+	a(result.childNodes[1].nodeName, 'STRONG');
+	a(result.childNodes[1].childNodes.length, 3);
+	a(result.childNodes[1].childNodes[0].data, 'marek ');
+	a(result.childNodes[1].childNodes[1].data, 'fura');
+	a(result.childNodes[1].childNodes[2].data, ' zegarek');
+	a(result.childNodes[2].data, ' zagalo ');
+	a(result.childNodes[3].nodeName, 'P');
+	a(result.childNodes[3].childNodes.length, 1);
+	a(result.childNodes[3].childNodes[0].data, 'razdwa');
+};
